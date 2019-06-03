b43fe31fd65c3df8a9416abde6f98856ec6fbf9e
darwindu <252921602@qq.com>
2019-06-03 19:36:59 +0800

    update conf.py

--- docs/en/source/conf.py
+++ docs/en/source/conf.py
@@ -67,3 +67,9 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Language to be used for generating the HTML full-text search index.
+# Sphinx supports the following languages:
+#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
+#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
+html_search_language = 'en'
